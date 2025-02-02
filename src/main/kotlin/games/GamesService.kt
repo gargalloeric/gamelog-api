9c31576d3ba14cda11361262ece05a5c7f0f05a1
gargalloeric <erickmontes555@gmail.com>
2025-02-02 18:44:50 +0100

    feat: add igdb.com api request

--- src/main/kotlin/games/GamesService.kt
+++ src/main/kotlin/games/GamesService.kt
@@ -2,22 +2,26 @@ package com.mocosoft.games
 
 import com.mocosoft.games.models.GameList
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 
 interface GamesService {
     suspend fun getGames(pageNumber: Long?, pageSize: Long?): List<GameList>
 }
 
-class GameServiceImpl(val httpClient: HttpClient) : GamesService {
+class GameServiceImpl(private val httpClient: HttpClient) : GamesService {
+
+    private val endpoint: String = "games"
+
     override suspend fun getGames(pageNumber: Long?, pageSize: Long?): List<GameList> {
         requireNotNull(pageNumber)
         requireNotNull(pageSize)
 
+        val games: List<GameList> = httpClient.post(endpoint) {
+            setBody("fields name;limit $pageSize;offset $pageNumber;sort total_rating desc;where category = (0, 8, 9);")
+        }.body()
 
-
-        val list = listOf(
-            GameList(id = 1, name = "Test", cover = 2)
-        )
-
-        return list
+        return games
     }
 }
