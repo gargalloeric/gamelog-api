a4f3bd947cf094e4ed40b30d2a1cad329110b905
DarkPunk <alexbeltransalas@gmail.com>
2025-02-02 20:13:10 +0100

    feat(timetobeat): get time to beat functionality added

---
+++ src/main/kotlin/timetobeat/models/IGDBGameDuration.kt
@@ -0,0 +1,11 @@
+package com.mocosoft.timetobeat.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class IGDBGameDuration(
+    val id: Long,
+    val hastily: Long,
+    val normally: Long,
+    val completely: Long
+)
