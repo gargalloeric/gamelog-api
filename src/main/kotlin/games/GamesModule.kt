d1471e130e3cc42c80b86f00f7ca273d6f104227
gargalloeric <erickmontes555@gmail.com>
2025-02-08 16:08:31 +0100

    refactor(koin): add cover service injection in GameServiceImpl

--- src/main/kotlin/games/GamesModule.kt
+++ src/main/kotlin/games/GamesModule.kt
@@ -3,5 +3,5 @@ package com.mocosoft.games
 import org.koin.dsl.module
 
 val gamesModule = module {
-    single<GamesService> { GameServiceImpl(get()) }
+    single<GamesService> { GameServiceImpl(get(), get()) }
 }
