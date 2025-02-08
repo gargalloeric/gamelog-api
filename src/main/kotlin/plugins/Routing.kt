21fcfb8371ffd1f881b1f3625a4b3153ff4084b8
gargalloeric <erickmontes555@gmail.com>
2025-02-08 17:07:28 +0100

    refactor(routing): remove unnecessary routes

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,8 +1,6 @@
 package com.mocosoft.plugins
 
-import com.mocosoft.routes.configureCoverRoutes
 import com.mocosoft.routes.configureGamesRoutes
-import com.mocosoft.routes.configureTimeToBeatRoutes
 import io.ktor.server.application.Application
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.routing.routing
@@ -10,8 +8,6 @@ import io.ktor.server.routing.routing
 fun Application.configureRouting() {
     routing {
         configureGamesRoutes()
-        configureCoverRoutes()
-        configureTimeToBeatRoutes()
         staticResources("/static", "static")
     }
 }
